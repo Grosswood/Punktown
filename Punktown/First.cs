bdfd676f6a290bf0ecfc9e326ad60141076e4579
Grosswood <mozge4ok@gmail.com>
2014-09-20 17:07:17 +0600

    Added random functions

--- Punktown/First.cs
+++ Punktown/First.cs
@@ -8,8 +8,26 @@ namespace Punktown
 {
     class First
     {
+        static int d(int dice)
+        {
+//Private random            
+            Random random = new Random();
+            return (random.Next(1, dice));
+        }
+
+        static int D(int dice)
+        {
+//Public random
+            Random random = new Random();
+            int diceValue = random.Next(1, dice);
+            Console.WriteLine("d" + dice + " rolls for " + diceValue);
+            return (diceValue);
+        }
+
         static void Main(string[] args)
         {
+            Console.WriteLine(d(10));
+            D(20);
         }
     }
 }
